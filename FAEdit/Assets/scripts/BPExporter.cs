381c26f91ae9c195bf4143bf79dd83eedaded670
John Werner <mooilo.ueu@gmail.com>
2015-02-26 22:10:27 -0800

    Improved exporter formatting.

--- FAEdit/Assets/scripts/BPExporter.cs
+++ FAEdit/Assets/scripts/BPExporter.cs
@@ -9,11 +9,11 @@ public static class BPExporter
 	{
 		using (StreamWriter writer = new StreamWriter(blueprintFile.OpenWrite(), Encoding.UTF8))
 		{
-			SaveLuaValue(blueprint.Get(""), writer);
+			SaveLuaValue(blueprint.Get(""), writer, "	");
 		}
 	}
 
-	private static void SaveLuaValue(LuaValue luaValue, StreamWriter writer)
+	private static void SaveLuaValue(LuaValue luaValue, StreamWriter writer, string tableIndent)
 	{
 		switch (luaValue.LVT)
 		{
@@ -24,13 +24,14 @@ public static class BPExporter
 		}
 		case LuaValueType.LVT_String:
 		{
-			writer.Write('"');
+			writer.Write('\'');
 
 			string sanitizedString = luaValue.StringValue;
+			sanitizedString.Replace("'", "\\'");
 			sanitizedString.Replace("\"", "\\\"");
 
 			writer.Write(sanitizedString);
-			writer.Write("\"");
+			writer.Write('\'');
 			break;
 		}
 		case LuaValueType.LVT_Float:
@@ -57,13 +58,15 @@ public static class BPExporter
 			ICollection<string> tableKeys = table.Keys;
 			foreach (string tableKey in tableKeys)
 			{
+				writer.Write(tableIndent);
+
 				writer.Write(tableKey);
 				writer.Write(" = ");
 
 				LuaValue tableValue = null;
 				if (table.TryGetValue(tableKey, out tableValue))
 				{
-					SaveLuaValue(tableValue, writer);
+					SaveLuaValue(tableValue, writer, tableIndent + "	");
 				}
 				else
 				{
@@ -73,6 +76,7 @@ public static class BPExporter
 				writer.Write(",\n");
 			}
 
+			writer.Write(tableIndent.Substring(1));
 			writer.Write("}");
 
 			break;
